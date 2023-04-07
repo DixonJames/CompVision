24ae1d1a0afa55656a936d1fa1926b222b8173a3
DixonJames <39742785+DixonJames@users.noreply.github.com>
2023-04-07 12:02:46 +0100

    added skeleton model sources

---
+++ dl_pose.sh
@@ -0,0 +1,2 @@
+wget  http://posefs1.perception.cs.cmu.edu/OpenPose/models/pose/coco/pose_iter_440000.caffemodel
+wget https://raw.githubusercontent.com/CMU-Perceptual-Computing-Lab/openpose/2034f9c07526b885c16ba94eec4a4bd6cc8787c3/models/pose/coco/pose_deploy_linevec.prototxt
